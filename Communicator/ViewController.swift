693986416c9cfef856589996adfea24f1aa06818
Satyam Sovan Mishra <msatyamsovan@gmail.com>
2023-08-09 14:26:12 +0530

    Add base files

--- Communicator/ViewController.swift
+++ Communicator/ViewController.swift
@@ -7,10 +7,25 @@
 
 import UIKit
 
+extension UITextField {
+    func addBottomBorder(){
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
+        bottomLine.backgroundColor = UIColor.white.cgColor
+        borderStyle = .none
+        layer.addSublayer(bottomLine)
+    }
+}
+
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+//        self.textField.addBottomBorder()
         // Do any additional setup after loading the view.
     }
 
